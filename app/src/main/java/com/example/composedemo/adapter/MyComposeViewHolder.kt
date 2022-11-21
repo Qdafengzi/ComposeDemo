cc8e5e64d335b7c4c2bb2fbc800b8dd5e599f9b8
qdafengzi <qdafengzi@foxmail.com>
2022-11-21 20:48:37 +0800

    rv 添加YouTube视频 和其他item

--- app/src/main/java/com/example/composedemo/adapter/MyComposeViewHolder.kt
+++ app/src/main/java/com/example/composedemo/adapter/MyComposeViewHolder.kt
@@ -1,13 +1,18 @@
 package com.example.composedemo.adapter
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.recyclerview.widget.RecyclerView
@@ -17,78 +22,36 @@ class MyComposeViewHolder(
 ) : RecyclerView.ViewHolder(composeView) {
     fun bind(input: String) {
         composeView.setContent {
-            Column(modifier = Modifier.padding(top = 4.dp)) {
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = input,
-                    textAlign = TextAlign.Center,
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.SemiBold
+            Row(
+                modifier = Modifier
+                    .padding(top = 10.dp, bottom = 10.dp)
+                    .fillMaxWidth()
+                    .background(color = Color.White, shape = RoundedCornerShape(9.dp))
+            ) {
+                Image(
+                    painter = painterResource(id = com.example.composedemo.R.drawable.sheep),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .weight(1f)
+                        .aspectRatio(1f)
                 )
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp)
-                        .background(color = Color.Magenta)
-                ) {
-                    Row(modifier = Modifier.fillMaxWidth()) {
-                        Text(
-                            modifier = Modifier.weight(1f),
-                            text = input,
-                            textAlign = TextAlign.Center,
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.SemiBold
-                        )
-                        Text(
-                            modifier = Modifier.weight(1f),
-                            text = input,
-                            textAlign = TextAlign.Center,
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.SemiBold
-                        )
-                        Text(
-                            modifier = Modifier.weight(1f),
-                            text = input,
-                            textAlign = TextAlign.Center,
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.SemiBold
-                        )
 
-
-                    }
-                }
-
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp)
-                        .background(color = Color.Magenta)
-                ) {
-                    Row(modifier = Modifier.fillMaxWidth()) {
-                        Text(
-                            modifier = Modifier.weight(1f),
-                            text = input,
-                            textAlign = TextAlign.Center,
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.SemiBold
-                        )
-                        Text(
-                            modifier = Modifier.weight(1f),
-                            text = input,
-                            textAlign = TextAlign.Center,
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.SemiBold
-                        )
-                        Text(
-                            modifier = Modifier.weight(1f),
-                            text = input,
-                            textAlign = TextAlign.Center,
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.SemiBold
-                        )
-
-
-                    }
+                Column(modifier = Modifier.weight(2f).padding(start = 10.dp)) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = input,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "今日朝鲜发布了第一个核弹在太平洋,今日朝鲜发布了第一个核弹在太平洋,今日朝鲜发布了第一个核弹在太平洋",
+                        fontSize = 14.sp,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                    )
                 }
             }
         }
--- app/src/main/java/com/example/composedemo/ui/page/template/YoutubeRvPage.kt
+++ app/src/main/java/com/example/composedemo/ui/page/template/YoutubeRvPage.kt
@@ -1,15 +1,21 @@
 package com.example.composedemo.ui.page.template
 
 import android.view.LayoutInflater
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavHostController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.composedemo.R
-import com.example.composedemo.adapter.TestAdapter
+import com.example.composedemo.adapter.YoutubeListAdapter
 import com.example.composedemo.data.VideoEntity
 import com.example.composedemo.databinding.ActivityYoutubeListRvBinding
 import com.example.composedemo.ui.widget.CommonToolbar
@@ -46,40 +52,27 @@ fun YoutubeRvPage(navCtrl: NavHostController, title: String) {
     CommonToolbar(navCtrl, title) {
         val lifecycleOwner = LocalLifecycleOwner.current
         XLogger.d("---------->CommonToolbar")
-        AndroidView(factory = { context ->
-            val binding = DataBindingUtil.inflate<ActivityYoutubeListRvBinding>(
-                LayoutInflater.from(context),
-                R.layout.activity_youtube_list_rv,
-                null,
-                false
-            )
-            binding.rv.setHasFixedSize(true)
+        AndroidView(
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp, top = 12.dp)
+                .fillMaxWidth()
+                .background(color = Color.White),
+            factory = { context ->
+                val binding = DataBindingUtil.inflate<ActivityYoutubeListRvBinding>(
+                    LayoutInflater.from(context),
+                    R.layout.activity_youtube_list_rv,
+                    null,
+                    false
+                )
+                binding.rv.setHasFixedSize(true)
 
-            val mLayoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context)
-            binding.rv.layoutManager = mLayoutManager
+                val mLayoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context)
+                binding.rv.layoutManager = mLayoutManager
 
+                val youtubeListAdapter = YoutubeListAdapter(list, lifecycleOwner.lifecycle)
+                binding.rv.adapter = youtubeListAdapter
 
-//            val goodsAdapter = RvGoodsAdapter()
-//            val goodsList = list.filter {
-//                it.videoId.isNullOrBlank()
-//            }
-//
-//            goodsAdapter.goodsList = goodsList as MutableList<VideoEntity>
-//
-//           val videoList =  list.filter {
-//                !it.videoId.isNullOrBlank()
-//            }
-//
-//            val rvVideoAdapter= RvVideoAdapter( lifecycleOwner.lifecycle)
-//            rvVideoAdapter.videoList = videoList as MutableList<VideoEntity>
-//
-//            val adapter = ConcatAdapter(goodsAdapter,rvVideoAdapter)
-//            binding.rv.adapter = adapter
-
-            val testAdapter = TestAdapter(list,lifecycleOwner.lifecycle)
-            binding.rv.adapter = testAdapter
-
-            binding.root
-        })
+                binding.root
+            })
     }
 }
