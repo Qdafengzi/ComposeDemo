ae9fa938beea4125ddc020cb46959d3dd6fe49ac
Zongfeng Qiu <qiuzongfeng@sunseaaiot.com>
2022-11-22 13:20:20 +0800

    底部导航的颜色修改

--- app/src/main/java/com/example/composedemo/ui/page/common/Commons.kt
+++ app/src/main/java/com/example/composedemo/ui/page/common/Commons.kt
@@ -8,7 +8,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -28,16 +30,24 @@ fun BottomNavBarView(navCtrl: NavHostController) {
         val navBackStackEntry by navCtrl.currentBackStackEntryAsState()
         val currentDestination = navBackStackEntry?.destination
         bottomNavList.forEach { screen ->
+            val isSelected = currentDestination?.hierarchy?.any { it.route == screen.routeName } == true
             BottomNavigationItem(
                 modifier = Modifier.background(AppTheme.colors.themeUi),
                 icon = {
                     Icon(
                         imageVector = screen.icon,
-                        contentDescription = null
+                        contentDescription = null,
+                        tint = if(isSelected) Color.White else  Color(0xA8F5F8F5)
                     )
                 },
-                label = { Text(text = stringResource(screen.stringId)) },
-                selected = currentDestination?.hierarchy?.any { it.route == screen.routeName } == true,
+                label = {
+                    Text(
+                        text = stringResource(screen.stringId),
+                        color = if (isSelected) Color.White else  Color(0xA8F5F8F5),
+                        fontWeight = if (isSelected) FontWeight.SemiBold else FontWeight.Normal
+                    )
+                },
+                selected = isSelected,
                 onClick = {
                     println("BottomNavView当前路由 ===> ${currentDestination?.hierarchy?.toList()}")
                     println("当前路由栈 ===> ${navCtrl.graph.nodes}")
